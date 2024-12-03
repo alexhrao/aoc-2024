90fc4bb96f6fea2661949f5b20411500a5fdc6a6
Alex Rao <alexhrao@gmail.com>
2024-12-02 21:41:22 -0600

    Improve day1 performance

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,6 @@
-use aoc_runner_derive::{aoc_generator, aoc};
+use std::collections::HashMap;
+
+use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day1)]
 pub fn gen(input: &str) -> (Vec<u32>, Vec<u32>) {
@@ -13,20 +15,26 @@ pub fn gen(input: &str) -> (Vec<u32>, Vec<u32>) {
 }
 
 #[aoc(day1, part1)]
-pub fn solve_p1((left, right): &(Vec<u32>, Vec<u32>)) -> u32 {
+pub fn part1((left, right): &(Vec<u32>, Vec<u32>)) -> u32 {
     let (mut left, mut right) = (left.clone(), right.clone());
     left.sort_unstable();
     right.sort_unstable();
-    left.into_iter().zip(right)
+    left.into_iter()
+        .zip(right)
         .map(|(left, right)| left.abs_diff(right))
         .sum()
 }
 
 #[aoc(day1, part2)]
-pub fn solve_p2((left, right): &(Vec<u32>, Vec<u32>)) -> usize {
+pub fn part2((left, right): &(Vec<u32>, Vec<u32>)) -> u32 {
+    let mut right_count: HashMap<&u32, u32> = HashMap::new();
+    for r in right {
+        let c = right_count
+            .entry(r)
+            .or_default();
+        *c += 1;
+    }
     left.iter()
-        .map(|l| {
-            (*l as usize) * right.iter().filter(|&r| l == r).count()
-        })
+        .filter_map(|l| right_count.get(l).map(|r| l * r))
         .sum()
 }
